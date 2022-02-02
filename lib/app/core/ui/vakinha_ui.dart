37dcd117c99f684b596a631cec3a2183bbb96d56
HKprogram <ricknogueira1231@gmail.com>
2022-02-02 18:25:33 -0300

    31:00

---
+++ lib/app/core/ui/vakinha_ui.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class VakinhaUI {
+  VakinhaUI._();
+
+  static final ThemeData theme = ThemeData(
+    primaryColor: const Color(0xFF007D21),
+    primaryColorDark: const Color(0xFF00343F),
+    fontFamily: 'mplus1',
+  );
+}
