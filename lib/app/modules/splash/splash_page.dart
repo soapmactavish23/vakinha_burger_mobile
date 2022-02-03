ef3be88501cdb6e22ae6964f3cc846c1961fcf31
HKprogram <ricknogueira1231@gmail.com>
2022-02-03 18:58:44 -0300

    Criação do buttao

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vakinha_burguer_mobile/app/core/ui/widgets/vakinha_button.dart';
 import 'package:vakinha_burguer_mobile/app/core/ui/widgets/vakinha_textformfield.dart';
 
 class SplashPage extends StatefulWidget {
@@ -9,17 +10,33 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  logar() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Splash'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: VakinhaTextformfield(
-          label: 'Text Form',
-        ),
+      body: Column(
+        children: const [
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: VakinhaTextformfield(
+              label: 'Text Form',
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          VakinhaButton(
+            label: 'Entrar',
+            onPressed: () {},
+          )
+        ],
       ),
     );
   }
